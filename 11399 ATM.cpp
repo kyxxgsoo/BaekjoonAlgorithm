7484e5ab36babe741cf264112096dd47bc6bb3dc
KyungsooLee <jmtkd9196@gmail.com>
2022-03-09 23:08:33 +0900

    Baekjoon 11399
    [Greedy]
    [Sort]

---
+++ 11399 ATM.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int n;
+int arr[1001];
+
+int main()
+{
+	vector<int> v;
+	int ans = 0;
+	cin >> n;
+	for(int i = 0; i < n; i++)
+	{
+		int num;
+		cin >> num;
+		v.push_back(num);
+	}
+
+	sort(v.begin(), v.end());
+
+	arr[0] = v[0];
+	ans = v[0];
+	for (int i = 1; i < n; i++)
+	{
+		ans += v[i] + arr[i - 1];
+		arr[i] += v[i] + arr[i - 1];
+	}
+
+	cout << ans;
+}
