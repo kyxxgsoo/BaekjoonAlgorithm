c8c3cd811594fe9a6493cc941a77b786e53c588a
KyungsooLee <21362256+jmtkd9196@users.noreply.github.com>
2022-08-25 03:27:59 +0900

    [Greedy] / [Sorting]

--- C++/11399 ATM.cpp
+++ C++/11399 ATM.cpp
@@ -8,27 +8,21 @@ using namespace std;
 int n;
 int arr[1001];
 
-int main()
-{
+int main() {
 	vector<int> v;
 	int ans = 0;
 	cin >> n;
-	for(int i = 0; i < n; i++)
-	{
+	for(int i = 0; i < n; i++) {
 		int num;
 		cin >> num;
 		v.push_back(num);
 	}
-
 	sort(v.begin(), v.end());
-
 	arr[0] = v[0];
 	ans = v[0];
-	for (int i = 1; i < n; i++)
-	{
+	for (int i = 1; i < n; i++) {
 		ans += v[i] + arr[i - 1];
 		arr[i] += v[i] + arr[i - 1];
 	}
-
 	cout << ans;
 }
