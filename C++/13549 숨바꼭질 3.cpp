1890f8957bf839f6c5cbe2177a5c9af8f66ef856
KyungsooLee <21362256+jmtkd9196@users.noreply.github.com>
2022-08-25 03:34:16 +0900

    [Graph] / [BFS] / [Dijkstra] / [0-1 BFS]

--- C++/13549 숨바꼭질 3.cpp
+++ C++/13549 숨바꼭질 3.cpp
@@ -6,8 +6,7 @@
 
 using namespace std;
 
-struct Node
-{
+struct Node {
 	int x;
 	int cnt;
 };
@@ -16,44 +15,31 @@ int n, k;
 bool visit[100001];
 int arr[100001] = { 99999 };
 
-void bfs()
-{
+void bfs() {
 	queue<Node> q;
 	q.push({ n, 0 });
 	visit[n] = true;
-
 	arr[n] = 0;
-	
 
-	while (!q.empty())
-	{
+	while (!q.empty()) {
 		int curX = q.front().x;
 		int curCnt = q.front().cnt;
 		q.pop();
 
-		for (int i = 0; i < 3; i++)
-		{
+		for (int i = 0; i < 3; i++) {
 			int nextX = curX;
 			int nextCnt = curCnt;;
-			if (i == 0)
-			{
+			if (i == 0) {
 				nextX--;
 				nextCnt++;
-			}
-			else if (i == 1)
-			{
+			} else if (i == 1) {
 				nextX++;
 				nextCnt++;
-			}
-			else if (i == 2)
-			{
+			} else if (i == 2) {
 				nextX *= 2;
 			}
-
-			if (0 <= nextX && nextX <= 100000)
-			{
-				if (!visit[nextX] || nextCnt < arr[nextX])
-				{
+			if (0 <= nextX && nextX <= 100000) {
+				if (!visit[nextX] || nextCnt < arr[nextX]) {
 					q.push({ nextX, nextCnt });
 					visit[nextX] = true;
 					arr[nextX] = nextCnt;
@@ -63,11 +49,8 @@ void bfs()
 	}
 }
 
-int main()
-{
+int main() {
 	cin >> n >> k;
-
 	bfs();
-
 	cout << arr[k];
 }
