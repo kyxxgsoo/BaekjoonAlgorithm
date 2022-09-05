4d6a6452caca74aa3250467815cdae0189405785
KyungsooLee <jmtkd9196@gmail.com>
2022-09-06 02:45:59 +0900

    [Implementation]

---
+++ Swift/2742 기찍 N.swift
@@ -0,0 +1,14 @@
+//
+//  2742 기찍 N.swift
+//  Swift
+//
+//  Created by Kyungsoo Lee on 2022/09/06.
+//
+
+import Foundation
+
+let N = Int(readLine()!)!
+
+for i in (1...N).reversed() {
+    print(i)
+}
