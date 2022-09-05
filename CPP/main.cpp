6cb473598e417d379eccac54ce29bb38868e0e8c
KyungsooLee <jmtkd9196@gmail.com>
2022-09-05 18:57:26 +0900

    Rename Cpp directory & Link Xcode project file

---
+++ CPP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Cpp
+//
+//  Created by Kyungsoo Lee on 2022/09/05.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
