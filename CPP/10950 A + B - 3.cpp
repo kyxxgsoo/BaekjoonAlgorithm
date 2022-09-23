218631de210d6fe66da7ac9ae613cb2f1acbef4b
KyungsooLee <jmtkd9196@gmail.com>
2022-09-23 16:57:42 +0900

    [Mathematics] / [Implementation] / [Arithmetic]

---
+++ CPP/10950 A + B - 3.cpp
@@ -0,0 +1,21 @@
+//
+//  10950 A + B - 3.cpp
+//  Cpp
+//
+//  Created by Kyungsoo Lee on 2022/09/23.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    
+    for(int i = 0; i < t; i++) {
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << "\n";
+    }
+}
