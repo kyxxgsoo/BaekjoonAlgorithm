d0892d23368ff4ad66c84daf72266036afd0ebde
KyungsooLee <jmtkd9196@gmail.com>
2023-10-14 20:29:59 +0900

    [Graph] / [BFS]

---
+++ Python/2468 안전 영역.py
@@ -0,0 +1,11 @@
+import sys
+from collections import deque
+
+n = 0
+graph = []
+visited = []
+
+n = int(input())
+visited = [[False for _ in range(n)] for _ in range(n)]
+for _ in range:
+    graph.append(list(map(int, sys.stdin.readline().split(" "))))
