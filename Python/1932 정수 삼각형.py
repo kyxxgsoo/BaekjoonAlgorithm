e4a4b5536518d745cb7e2dc3789a40917d83f17e
KyungsooLee <jmtkd9196@gmail.com>
2023-11-09 19:01:04 +0900

    [Dynamic Programming]

--- Python/1932 정수 삼각형.py
+++ Python/1932 정수 삼각형.py
@@ -12,6 +12,11 @@ dp[0][0] = a[0][0]
 
 for i in range(n):
     for j in range(i + 1):
+        # # j가 0인 경우 j-1은 -1을 접근하게 되기 때문에 에러가 안나는데... 나중에 문제가 발생할 여지가 있음.
+        # if j == 0:
+        #     dp[i][j] = dp[i - 1][0] + a[i][j]
+        # else:
+        #     dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - 1]) + a[i][j]
         dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - 1]) + a[i][j]
 
 ans = 0
