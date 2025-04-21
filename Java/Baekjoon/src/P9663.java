e2abb2a7d0137575cff2c3a31c2ea2499fdf5ea6
KyungsooLee <jmtkd9196@gmail.com>
2025-04-21 15:19:34 +0900

    N-Queen

--- Java/Baekjoon/src/P9663.java
+++ Java/Baekjoon/src/P9663.java
@@ -21,28 +21,22 @@ public class P9663 {
     }
 
     static boolean isPossible(int x, int y) {
-        // 현재 y보다 윗 행에 대한 값들에 대해서 겹치는게 없는지 비교
         for (int i = 0; i < y; i++) {
-            // 같은 열 or 같은 대각선에 겹치는게 없는지 비교
-            if (board[i] == x || Math.abs(board[i] - x) == Math.abs(i - y)) {
+            if (board[i] == x || Math.abs((y - i)) == Math.abs(x - board[i])) {
                 return false;
             }
         }
         return true;
     }
 
-    static void recursion(int y) throws IOException {
+    static void recursion(int y) {
 
-        // 1. 기저 사례
         if (y == N) {
             answer++;
             return;
         }
 
-        // 2. 값 초기화
 
-
-        // 3. 경우의 수(재귀)
         for (int x = 0; x < N; x++) {
             if (isPossible(x, y)) {
                 board[y] = x;
@@ -51,7 +45,9 @@ public class P9663 {
         }
 
 
+
     }
+
 
     public static void main(String[] args) throws IOException {
         init();
