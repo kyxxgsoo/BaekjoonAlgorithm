6443cb68963248b2a21e1e2d1a58d67fdd58e7d5
KyungsooLee <jmtkd9196@gmail.com>
2025-07-31 21:30:13 +0900

    기본 포멧

---
+++ Java/Baekjoon/src/Default.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Default {
+
+    static BufferedReader br;
+    static BufferedWriter bw;
+    static StringTokenizer st;
+
+    static void init() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        st = new StringTokenizer(br.readLine());
+    }
+
+    static void solve() throws IOException {
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        init();
+        solve();
+
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+}
